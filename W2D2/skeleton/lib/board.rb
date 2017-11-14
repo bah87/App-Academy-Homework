c36f72dc93c1b46fc27edf37ce069f40215871fb
Brendan Higgins <bhiggins033@gmail.com>
2017-11-13 22:51:22 -0500

    Final mancala commit

--- W2D2/skeleton/lib/board.rb
+++ W2D2/skeleton/lib/board.rb
@@ -2,6 +2,15 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1, @name2 = name1, name2
+    @cups = Array.new(14) { [] }
+    fill_cups
+  end
+  
+  def fill_cups
+    @cups.each_with_index do |arr, idx|
+      @cups[idx] += [:stone, :stone, :stone, :stone] unless idx == 6 || idx == 13
+    end
   end
 
   def place_stones
@@ -9,9 +18,34 @@ class Board
   end
 
   def valid_move?(start_pos)
+    raise "Invalid starting cup" unless start_pos.between?(1,14)
   end
 
   def make_move(start_pos, current_player_name)
+    i = start_pos
+    until @cups[start_pos].empty?
+      i += 1
+      i = 0 if i == 14
+      if current_player_name == @name1
+        @cups[i] << @cups[start_pos].pop unless i == 13
+      else 
+        @cups[i] << @cups[start_pos].pop unless i == 6
+      end 
+    end 
+    
+    render
+    next_turn(i)
+    if end_on_players_points_cup?(current_player_name, i)
+      :prompt
+    elsif @cups[i].length == 1 
+      :switch
+    elsif @cups[i].length > 1
+      i 
+    end
+  end
+  
+  def end_on_players_points_cup?(name, i)
+    (name == @name1) && (i == 6) || (name == @name2) && (i == 13)
   end
 
   def next_turn(ending_cup_idx)
@@ -27,8 +61,16 @@ class Board
   end
 
   def one_side_empty?
+    @cups[0..5].all? { |cup| cup.empty? } || @cups[7..12].all? { |cup| cup.empty? }
   end
 
   def winner
+    if @cups[6].length == @cups[13].length
+      :draw
+    elsif @cups[6].length > @cups[13].length
+      @name1
+    else 
+      @name2
+    end
   end
 end
