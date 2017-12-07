e354e024ba87a186204bbf37a330135d2f285058
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-12-07 09:04:16 -0500

    Complete hw

--- W5D4/javascript_hw.js
+++ W5D4/javascript_hw.js
@@ -112,7 +112,7 @@ function sumOfNPrimes(n) {
 // Part II
 function titleize(names, callback) {
   let titleized = names.map(name => `Mx. ${name} Jingleheimer Schmidt`);
-  callback(titleized)
+  callback(titleized);
 }
 
 // Invocation
@@ -127,18 +127,32 @@ function Elephant (name, height, tricks) {
 }
 
 Elephant.prototype.trumpet = function () {
-  console.log(`${this.name} the elephant goes phrRRRRRRRRRRR!!!!!!!`)
-}
+  console.log(`${this.name} the elephant goes phrRRRRRRRRRRR!!!!!!!`);
+};
 
 Elephant.prototype.grow = function () {
   this.height += 12;
-}
+};
 
 Elephant.prototype.addTrick = function (trick) {
   this.tricks.push(trick);
-}
+};
 
 Elephant.prototype.play = function () {
   trick_index = Math.floor(Math.random()*(this.tricks.length));
-  console.log(`${this.trick}`)
-}
+  console.log(`${this.name} is ${this.tricks[trick_index]}`);
+};
+
+Elephant.paradeHelper = function (elephant) {
+  console.log(`${elephant.name} is trotting by!`);
+};
+
+function dinerBreakfast() {
+  let order = "I'd like cheesy scrambled eggs please.";
+  console.log(order);
+
+  return function (food) {
+    order = `${order.slice(0, order.length - 8)} and ${food} please.`;
+    console.log(order);
+  };
+};
