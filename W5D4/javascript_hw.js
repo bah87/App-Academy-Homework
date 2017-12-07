acadb50beb773789770fcbbe5e1258ec46551945
Brendan <bhiggins033@gmail.com>
2017-12-07 07:13:32 -0500

    Second hw commit JS

--- W5D4/javascript_hw.js
+++ W5D4/javascript_hw.js
@@ -112,8 +112,33 @@ function sumOfNPrimes(n) {
 // Part II
 function titleize(names, callback) {
   let titleized = names.map(name => `Mx. ${name} Jingleheimer Schmidt`);
-  callback(titleized);
-};
+  callback(titleized)
+}
 
+// Invocation
+// titleize(['Mary', 'Brian', 'Leo'], (names) => {
+//   names.forEach(name => console.log(name));
+// })
 
+function Elephant (name, height, tricks) {
+  this.name = name;
+  this.height = height;
+  this.tricks = tricks;
+}
 
+Elephant.prototype.trumpet = function () {
+  console.log(`${this.name} the elephant goes phrRRRRRRRRRRR!!!!!!!`)
+}
+
+Elephant.prototype.grow = function () {
+  this.height += 12;
+}
+
+Elephant.prototype.addTrick = function (trick) {
+  this.tricks.push(trick);
+}
+
+Elephant.prototype.play = function () {
+  trick_index = Math.floor(Math.random()*(this.tricks.length));
+  console.log(`${this.trick}`)
+}
