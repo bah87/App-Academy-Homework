b5875710444e1664c04f03de8864fa757db54547
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-12-19 08:29:43 -0500

    W7D2 HW

---
+++ W7D2/frontend/todo_redux.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+// Phase 1
+// function addLoggingToDispatch(store) {
+//   let storeDispatch = store.dispatch;
+//   return (action) => {
+//     console.log(store.getState());
+//     console.log(action);
+//     storeDispatch(action);
+//     console.log(store.getState());
+//   };
+// }
+
+// Phase 2
+// const addLoggingToDispatch = store => next => action => {
+//   console.log(store.getState());
+//   console.log(action);
+//   next(action);
+//   console.log(store.getState());
+// };
+// Phase 2
+// const applyMiddlewares = (store, ...middlewares) => {
+//   let dispatch = store.dispatch;
+//   middlewares.forEach((middleware) => {
+//     dispatch = middleware(store)(dispatch);
+//   });
+//
+//   return Object.assign({}, store, { dispatch });
+// };
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  let store = configureStore(preloadedState);
+
+  // Phase 1
+  // store.dispatch = addLoggingToDispatch(store);
+
+  // Phase 2
+  // store = applyMiddlewares(store, addLoggingToDispatch);
+
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
