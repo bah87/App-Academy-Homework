2227b22f3c89c219a9af2bc13eff8dd78acdb14a
Brendan <bhiggins033@gmail.com>
2017-12-20 01:08:10 -0500

    Fixed entry file

--- W7D3/giphy_search.jsx
+++ W7D3/giphy_search.jsx
@@ -2,3 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import GiphysIndex from './components/giphys_index';
+import * as APIUtil from './util/api_util';
+import { receiveSearchGiphys } from './actions/giphy_actions';
+
+// window.store = store;
+// window.fetchSearchGiphys = APIUtil.fetchSearchGiphys;
+// window.receiveSearchGiphys = receiveSearchGiphys;
+
+document.addEventListener('DOMContentLoaded', () => {
+	const store = configureStore();
+	const root = document.getElementById('root');
+	ReactDOM.render(<Root store={store} />, root);
+});
